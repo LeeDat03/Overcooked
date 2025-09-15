107ddd661d0cf6a417cb8c45dcbf1b568bc56e91
LeeDat03 <leedat104517@gmail.com>
2025-09-15 21:42:16 +0700

    add collider detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,29 +9,78 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameInput gameInput;
 
-    private InputActions inputActions; // TODO: seperate to other file
-
     private bool isWalking;
 
     private void Start()
     {
-        inputActions = new InputActions();
-        inputActions.Enable();
-
         isWalking = false;
     }
 
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(
+            transform.position,
+            transform.position + Vector3.up * playerHeight,
+            playerRadius,
+            moveDir,
+            moveDistance
+        );
+
+        if (!canMove)
+        {
+            // can not move toward
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(
+                transform.position,
+                transform.position + Vector3.up * playerHeight,
+                playerRadius,
+                moveDirX,
+                moveDistance
+            );
+
+            if (canMove)
+            {
+                // can move to the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(
+                    transform.position,
+                    transform.position + Vector3.up * playerHeight,
+                    playerRadius,
+                    moveDirZ,
+                    moveDistance
+                );
+
+                if (canMove)
+                {
+                    // can move to the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // can not move
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        Debug.Log(inputVector);
     }
 
     public bool IsWalking()
